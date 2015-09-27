2ed0e9d885e00215779373385ba51e9231bb11f0
1000ch <shogo.sensui@gmail.com>
2015-09-28 02:14:15 +0900

    wip

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 'use babel';
 
-import fs        from 'fs';
-import { Range } from 'atom';
-import polylint  from 'polylint';
+import fs from 'fs';
+import {Range} from 'atom';
+import polylint from 'polylint';
+
+let directory = null;
 
 export const activate = () => {
+
+  // install deps
   require("atom-package-deps").install("linter-polylint");
+
+  let directories = atom.project.getDirectories();
+
+  if (!directories.length) {
+    return;
+  }
+
+  directory = directories.shift();
 };
 
 export const provideLinter = () => {
 
   return {
-    grammarScopes: ['source.html'],
+    grammarScopes: ['text.html.basic'],
     scope: 'file',
     lintOnFly: true,
     lint: (editor) => {
 
+      if (!directory) {
+        return;
+      }
+
       let path = editor.getPath();
       let text = editor.getText();
 
-      return {
-        type: message.type,
-        text: message.text,
-        filePath: path,
-        range: range
-      };
-
+      polylint(text, {
+        root: directory
+      }).then(warnings => {
+        console.log(warnings);
+      }).catch(error => console.error(error));
     }
   };
 };
